1d52c576eb4096936c9b3639175af44b5a6728e2
kozera <kozerajd@gmail.com>
2024-06-24 09:52:09 -0400

    added documentation on demo, more cleanup

--- high_fidelity/hf_solver.py
+++ high_fidelity/hf_solver.py
@@ -11,20 +11,16 @@ import imageio
 
 class hf_solver:
     '''
-    This is a high-fidelity solver for time-evolving wave functions, used to generate training data and animations. 
+    This is a high-fidelity solver for time-evolving wave functions. 
 
     Methods:
-    1. get_ranges: Returns the space and time arrays
-    2. get_all_states: Returns all time-evolved states from a sim
-    3. get_run_time: Returns the runtme of a sim
     4. set_initial_state: Sets the initial state of the wavefunction
-    5. construct_hamiltonian: Generates the hamiltonian matrix. Input tag to choose your wavefunction, current takes: quantum harmonic oscillator ("QHO") or Gross-Pitaevskii ("GP")
-    6. run_solver: Runs the actual time-stepper
-    7. animation: Plays an animation in the iPython viewer or downloads an MP4
+    5. construct_hamiltonian: Generates the hamiltonian matrix. Input tag to choose your wavefunction, currently takes: quantum harmonic oscillator ("QHO") or Gross-Pitaevskii ("GP")
+    6. run_solver: Runs the time-stepper
+    7. animation: Plays an animation in the iPython viewer, downloads an MP4
     8. graph_energy: Generates an energy/time graph
     9. graph_particle_num: Generates a particle number/time graph
 
-    Questions can be sent to kozeraja@msu.edu
     '''
 
     def __init__(self, h_bar, mass, ang_freq, q, dx, x_limit, dt, t_final):
@@ -42,16 +38,7 @@ class hf_solver:
         self.x_range = np.linspace(-self.x_limit, self.x_limit, self.Nx)
 
         self.Nt = int((self.t_final/self.dt))
-        self.t_range = np.linspace(0, self.t_final, self.Nt)
-
-    def get_ranges(self):
-        return self.x_range, self.t_range
-    
-    def get_all_states(self):
-        return self.all_states
-    
-    def get_run_time(self):
-        return self.time_diff
+        self.t_range = np.linspace(0, self.t_final, self.Nt + 1)
     
     def set_initial_state(self, x0=0, sigma=1/(2)**(1/2), p=0):
         # x0 is the inital position
@@ -86,12 +73,13 @@ class hf_solver:
 
         self.hamiltonian = hamiltonian
     
-    def run_solver(self, gif = False, animation = True):
-        total_energies = np.ones(self.Nt)
-        total_kinetics = np.ones(self.Nt)
-        total_potentials = np.ones(self.Nt)
-        particle_num = np.ones(self.Nt)
-        all_states = np.zeros((self.Nt, *np.shape(self.hamiltonian)), dtype=complex)
+    def run_solver(self, snapshot = False, snapshot_freq = 1):
+        total_energies = np.ones(self.Nt + 1)
+        total_kinetics = np.ones(self.Nt + 1)
+        total_potentials = np.ones(self.Nt + 1)
+        particle_num = np.ones(self.Nt + 1)
+        all_states = np.zeros((self.Nt + 1, *np.shape(self.hamiltonian)), dtype=complex)
+        snapshots = []
         step = 0
 
         time_start = time.time()
@@ -103,6 +91,8 @@ class hf_solver:
 
             if t == 0:
                 final_state = self.initial_state.copy()
+                #The line below evolves the ground-state eigenvector of the equation. Set index to higher integers for excited states.
+                #final_state  = eigenvectors[:,0].T.copy()
 
                 final_state_norm = final_state/np.sqrt(np.dot(final_state,np.conj(final_state))*self.dx)
 
@@ -111,7 +101,7 @@ class hf_solver:
                 total_potentials[step] = np.dot(np.dot(np.conj(final_state_norm[:]),self.potential), final_state_norm[:])
                 particle_num[step] = sp.integrate.trapezoid(np.abs(final_state_norm[:])**2, self.x_range)
 
-                all_states[step, :, :] = final_state_norm[:]
+                all_states[step, :] = final_state_norm[:]
     
             else:
                 self.initial_state = final_state_norm[:].copy()
@@ -130,7 +120,10 @@ class hf_solver:
                 total_potentials[step] = np.dot(np.dot(np.conj(final_state_norm[:]),self.potential), final_state_norm[:])
                 particle_num[step] = sp.integrate.trapezoid(np.abs(final_state_norm[:])**2, self.x_range)
 
-                all_states[step, :, :] = final_state_norm[:]
+                all_states[step, :] = final_state_norm[:]
+
+            if step % snapshot_freq == 0 and snapshot == True:
+                snapshots.append(final_state_norm.copy())
 
             step += 1
 
@@ -143,9 +136,10 @@ class hf_solver:
         self.total_potentials = total_potentials
         self.particle_num = particle_num
         self.all_states = all_states
+        self.snapshots = snapshots
         self.time_diff = time_diff
         
-    def animation(self, show=False, save=False):
+    def animation(self, show = False, save = False, fps = 30):
         if show or save:
             fig, ax = plt.subplots(figsize=(10, 5))
             images = []
@@ -173,8 +167,7 @@ class hf_solver:
             plt.close(fig)
             
             if save:
-                imageio.mimsave('animation.mp4', images, fps=20)  # Adjust fps as needed
-
+                imageio.mimsave('animation.mp4', images, fps=fps)  # Adjust fps as needed
 
     def graph_energy(self):
         plt.plot(self.t_range, self.total_energies)
@@ -192,3 +185,27 @@ class hf_solver:
         plt.ylabel("Particle Number")
         plt.title("Particle Number")
         plt.savefig('Part_num.png')
+
+    def get_ranges(self):
+        return self.x_range, self.t_range
+    
+    def get_hamiltonian(self):
+        return self.hamiltonian
+
+    def get_kinetics(self):
+        return self.total_kinetics
+    
+    def get_potentials(self):
+        return self.total_potentials
+
+    def get_energies(self):
+        return self.total_energies
+    
+    def get_all_states(self):
+        return self.all_states
+    
+    def get_snapshots(self):
+        return self.snapshots
+    
+    def get_run_time(self):
+        return self.time_diff
